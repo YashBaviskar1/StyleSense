60907574daa2ebf50c311cd07922f3d1f31ba0d7
YashBaviskar1 <yashbav24@gmail.com>
2024-09-13 07:46:32 +0530

    add classifiy page

---
+++ static/js/classify.js
@@ -0,0 +1,35 @@
+function upload2() {
+    const fileUploader = document.querySelector('#image-upload');
+    const image = fileUploader.files[0];
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(image);
+
+  
+    fileReader.onload = (fileReaderEvent) => {
+        const profilePicture = document.querySelector('.image-preview');
+        profilePicture.style.backgroundImage = `url(${fileReaderEvent.target.result})`;
+        document.querySelector('#predict-button').click();
+    }
+  }
+async function classify(event){
+    event.preventDefault();
+    const formData = new FormData();
+    const fileUploader = document.querySelector('#image-upload');
+    const image = fileUploader.files[0];
+    formData.append('image', image);
+  
+    try {
+        const response = await fetch('/classify', {
+            method: 'POST',
+            body: formData,
+        });
+  
+        const result = await response.json();
+        const predictedLabel = result.category;
+        console.log(predictedLabel)
+    
+        document.querySelector('.classify').innerHTML = `Category: ${predictedLabel}`;
+    } catch (error) {
+        console.error('Error:', error);
+    }
+  }
